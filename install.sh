c83c6fc3205002131e3d9770b0623db2ff369461
Michał Popiel <131244219+Kogut01@users.noreply.github.com>
2025-10-09 10:59:02 +0200

    feat: add more links for arch

--- install.sh
+++ install.sh
@@ -102,6 +102,36 @@ elif [ "$OS" = "Linux" ]; then
     # Zsh
     link_file "$HOME/.dotfiles/src/dot_zsh/.zshrc" "$HOME/.zshrc"
 
+    # Greetd + ReGeet
+    sudo cp "$HOME/.dotfiles/src/dot_greetd_regreet/config.toml" "/etc/greetd/config.toml"
+    sudo cp "$HOME/.dotfiles/src/dot_greetd_regreet/regeet.toml" "/etc/greetd/regeet.toml" 
+    sudo cp "$HOME/.dotfiles/src/dot_greetd_regreet/hyprland.conf" "/etc/greetd/hyprland.conf"
+    sudo cp "$HOME/.dotfiles/src/dot_greetd_regreet/regeet.css" "/etc/greetd/regeet.css"
+
+    # Hyprland
+    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
+    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/00-base.conf" "$HOME/.config/hypr/hyprland/00-base.conf"
+    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/10-outputs.conf" "$HOME/.config/hypr/hyprland/10-outputs.conf"
+    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/20-inputs.conf" "$HOME/.config/hypr/hyprland/20-inputs.conf"
+    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/30-keybinds.conf" "$HOME/.config/hypr/hyprland/30-keybinds.conf"
+    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/40-windows.conf" "$HOME/.config/hypr/hyprland/40-windows.conf"
+    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/50-exec.conf" "$HOME/.config/hypr/hyprland/50-exec.conf"
+    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/60-ui.conf" "$HOME/.config/hypr/hyprland/60-ui.conf"
+
+    # Hyprpaper
+    link_file "$HOME/.dotfiles/src/dot_hypr/hyprpaper.conf" "$HOME/.config/hypr/hyprpaper.conf"
+
+    # Rofi
+    link_file "$HOME/.dotfiles/src/dot_rofi/config.rasi" "$HOME/.config/rofi/config.rasi"
+    link_file "$HOME/.dotfiles/src/dot_rofi/theme.rasi" "$HOME/.config/rofi/theme.rasi"
+
+    # Waybar
+    link_file "$HOME/.dotfiles/src/dot_waybar/config" "$HOME/.config/waybar/config"
+    link_file "$HOME/.dotfiles/src/dot_waybar/style.css" "$HOME/.config/waybar/style.css"
+    link_file "$HOME/.dotfiles/src/dot_waybar/scripts/wttr.py" "$HOME/.config/waybar/scripts/wttr.py"
+    link_file "$HOME/.dotfiles/src/dot_waybar/scripts/pacman-updates.sh" "$HOME/.config/waybar/scripts/pacman-updates.sh"
+    link_file "$HOME/.dotfiles/src/dot_waybar/modules.json" "$HOME/.config/waybar/modules.json"
+
 else
     echo "Nieznany system: $OS"
 fi
