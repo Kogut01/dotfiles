1097e74b2717b13efaa79d7e74a5bc8af0e821f1
Michał Popiel <131244219+Kogut01@users.noreply.github.com>
2025-10-09 21:43:18 +0200

    add: hyprpanel conf

--- src/dot_waybar/scripts/pacman-updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-updates=$(checkupdates | wc -l)
-aur_updates=$(yay -Quq --aur | wc -l)
-total=$((updates + aur_updates))
-if [ $total -gt 0 ]; then
-    echo "$total 📦"
-else
-    echo "OK"
-fi
