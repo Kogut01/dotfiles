f76375be6ccfff313480b48081fc57124a6fbe77
Michał Popiel <131244219+Kogut01@users.noreply.github.com>
2025-10-10 15:51:19 +0200

    fix: install script for arch

--- install.sh
+++ install.sh
@@ -16,16 +16,11 @@ install_brewfile() {
   brew bundle --file="$DOTFILES/src/dot_brew/Brewfile"
 }
 
-# install_arch_packages() {
-#   echo "Instaluję pakiety z pacman_packages.txt..."
-#   sudo pacman -Syu --needed --noconfirm $(< "$HOME/.dotfiles/src/dot_arch/pacman_packages.txt")
-# }
-
-# install_aur_packages() {
-#   echo "Instaluję pakiety z aur_packages.txt..."
-#   yay -Syu --needed --noconfirm $(< "$HOME/.dotfiles/src/dot_arch/aur_packages.txt")
-# }
-
+install_pacman_yay() {
+  echo "Instaluję pakiety z pacman i yay..."
+  sudo pacman -Syu --noconfirm < "$DOTFILES/src/dot_arch/pacman_packages.txt"
+  yay -Syu --noconfirm - < "$DOTFILES/src/dot_arch/yay_packages.txt"
+}
 
 if [ "$OS" = "Darwin" ]; then
     echo "Witaj w instalatorze .dotfiles dla systemu macOS"
@@ -66,11 +61,8 @@ if [ "$OS" = "Darwin" ]; then
 elif [ "$OS" = "Linux" ]; then
     echo "Witaj w instalatorze .dotfiles dla systemu Linux"
 
-    # Pacman packages
-    # install_arch_packages
-
-    # AUR packages
-    # install_aur_packages
+    # Pacman + Yay
+    install_pacman_yay
 
     # Bat
     link_file "$HOME/.dotfiles/src/dot_bat/config" "$HOME/.config/bat/config"
@@ -96,7 +88,10 @@ elif [ "$OS" = "Linux" ]; then
     link_file "$HOME/.dotfiles/src/dot_nvim/lua/config/kogut01.lua" "$HOME/.config/nvim/lua/config/kogut01.lua"
 
     # VsCode
-    xargs -n 1 code --install-extension < "$HOME/.dotfiles/src/dot_vscode/extensions.txt"
+    while read -r ext; do
+      [[ -n "$ext" ]] && code --no-sandbox --force --install-extension "$ext"
+      sleep 0.5
+    done < "$HOME/.dotfiles/src/dot_vscode/extensions.txt"
     link_file "$HOME/.dotfiles/src/dot_vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 
     # Zsh
@@ -104,33 +99,33 @@ elif [ "$OS" = "Linux" ]; then
 
     # Greetd + ReGeet
     sudo cp "$HOME/.dotfiles/src/dot_greetd_regreet/config.toml" "/etc/greetd/config.toml"
-    sudo cp "$HOME/.dotfiles/src/dot_greetd_regreet/regeet.toml" "/etc/greetd/regeet.toml" 
+    sudo cp "$HOME/.dotfiles/src/dot_greetd_regreet/regreet.toml" "/etc/greetd/regeet.toml" 
     sudo cp "$HOME/.dotfiles/src/dot_greetd_regreet/hyprland.conf" "/etc/greetd/hyprland.conf"
-    sudo cp "$HOME/.dotfiles/src/dot_greetd_regreet/regeet.css" "/etc/greetd/regeet.css"
+    sudo cp "$HOME/.dotfiles/src/dot_greetd_regreet/regreet.css" "/etc/greetd/regeet.css"
+    sudo cp "$HOME/.dotfiles/data/dot_wallpaper/wallpaper_dark.png" "/usr/share/backgrounds/greeter.png"
 
     # Hyprland
-    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
-    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/00-base.conf" "$HOME/.config/hypr/hyprland/00-base.conf"
-    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/10-outputs.conf" "$HOME/.config/hypr/hyprland/10-outputs.conf"
-    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/20-inputs.conf" "$HOME/.config/hypr/hyprland/20-inputs.conf"
-    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/30-keybinds.conf" "$HOME/.config/hypr/hyprland/30-keybinds.conf"
-    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/40-windows.conf" "$HOME/.config/hypr/hyprland/40-windows.conf"
-    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/50-exec.conf" "$HOME/.config/hypr/hyprland/50-exec.conf"
-    link_file "$HOME/.dotfiles/src/dot_hypr/hyprland/60-ui.conf" "$HOME/.config/hypr/hyprland/60-ui.conf"
+    link_file "$HOME/.dotfiles/src/dot_hyprland/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
+    link_file "$HOME/.dotfiles/src/dot_hyprland/hyprland/00-base.conf" "$HOME/.config/hypr/hyprland/00-base.conf"
+    link_file "$HOME/.dotfiles/src/dot_hyprland/hyprland/10-outputs.conf" "$HOME/.config/hypr/hyprland/10-outputs.conf"
+    link_file "$HOME/.dotfiles/src/dot_hyprland/hyprland/20-inputs.conf" "$HOME/.config/hypr/hyprland/20-inputs.conf"
+    link_file "$HOME/.dotfiles/src/dot_hyprland/hyprland/30-keybinds.conf" "$HOME/.config/hypr/hyprland/30-keybinds.conf"
+    link_file "$HOME/.dotfiles/src/dot_hyprland/hyprland/40-windows.conf" "$HOME/.config/hypr/hyprland/40-windows.conf"
+    link_file "$HOME/.dotfiles/src/dot_hyprland/hyprland/50-exec.conf" "$HOME/.config/hypr/hyprland/50-exec.conf"
+    link_file "$HOME/.dotfiles/src/dot_hyprland/hyprland/60-ui.conf" "$HOME/.config/hypr/hyprland/60-ui.conf"
 
     # Hyprpaper
-    link_file "$HOME/.dotfiles/src/dot_hypr/hyprpaper.conf" "$HOME/.config/hypr/hyprpaper.conf"
+    link_file "$HOME/.dotfiles/src/dot_hyprpaper/hyprpaper.conf" "$HOME/.config/hypr/hyprpaper.conf"
+
+    #Hyprpanel
+    link_file "$HOME/.dotfiles/src/dot_hyprpanel/config.json" "$HOME/.config/hyprpanel/config.json"
+    link_file "$HOME/.dotfiles/src/dot_hyprpanel/modules.scss" "$HOME/.config/hyprpanel/modules.scss"
+    link_file "$HOME/.dotfiles/src/dot_hyprpanel/modules.json" "$HOME/.config/hyprpanel/modules.json"
+
 
     # Rofi
     link_file "$HOME/.dotfiles/src/dot_rofi/config.rasi" "$HOME/.config/rofi/config.rasi"
     link_file "$HOME/.dotfiles/src/dot_rofi/theme.rasi" "$HOME/.config/rofi/theme.rasi"
-
-    # Waybar
-    link_file "$HOME/.dotfiles/src/dot_waybar/config" "$HOME/.config/waybar/config"
-    link_file "$HOME/.dotfiles/src/dot_waybar/style.css" "$HOME/.config/waybar/style.css"
-    link_file "$HOME/.dotfiles/src/dot_waybar/scripts/wttr.py" "$HOME/.config/waybar/scripts/wttr.py"
-    link_file "$HOME/.dotfiles/src/dot_waybar/scripts/pacman-updates.sh" "$HOME/.config/waybar/scripts/pacman-updates.sh"
-    link_file "$HOME/.dotfiles/src/dot_waybar/modules.json" "$HOME/.config/waybar/modules.json"
 
 else
     echo "Nieznany system: $OS"
