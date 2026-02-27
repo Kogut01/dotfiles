2ac2d01a1bf213b994aa9d1e6f4f41daa8bcb29a
Michał Popiel <131244219+Kogut01@users.noreply.github.com>
2026-02-27 16:42:16 +0100

    fix: installation script for macOS (#10)

--- install.sh
+++ install.sh
@@ -12,50 +12,61 @@ link_file() {
 }
 
 install_brewfile() {
+  echo ""
   echo "Instaluję pakiety z Brewfile..."
   brew bundle --file="$DOTFILES/src/dot_brew/Brewfile"
 }
 
 if [ "$OS" = "Darwin" ]; then
-    echo "Witaj w instalatorze .dotfiles dla systemu macOS"
+    echo "Witaj w instalatorze .dotfiles dla systemu macOS!"
+    echo "Czy chcesz kontynuować? (y/n)"
+
+    read -r response
+    if [ "$response" != "y" ]; then
+        echo ""
+        echo "Instalacja anulowana. Do zobaczenia!"
+        exit 1
+    fi
 
     # Brew files
     install_brewfile
 
     # Bat
-    link_file "$HOME/.dotfiles/src/dot_bat/config" "$HOME/.config/bat/config"
+    link_file "$DOTFILES/src/dot_bat/config" "$HOME/.config/bat/config"
 
     # Btop
-    link_file "$HOME/.dotfiles/src/dot_btop/btop.conf" "$HOME/.config/btop/btop.conf"
+    link_file "$DOTFILES/src/dot_btop/btop.conf" "$HOME/.config/btop/btop.conf"
 
     # Fastfetch
-    link_file "$HOME/.dotfiles/src/dot_fastfetch/config.jsonc" "$HOME/.config/fastfetch/config.jsonc"
-    link_file "$HOME/.dotfiles/src/dot_fastfetch/rooster.txt" "$HOME/.config/fastfetch/rooster.txt"
+    link_file "$DOTFILES/src/dot_fastfetch/config.jsonc" "$HOME/.config/fastfetch/config.jsonc"
+    link_file "$DOTFILES/src/dot_fastfetch/rooster.txt" "$HOME/.config/fastfetch/rooster.txt"
 
     # Ghostty
-    link_file "$HOME/.dotfiles/src/dot_ghostty/config" "$HOME/.config/ghostty/config"
+    link_file "$DOTFILES/src/dot_ghostty/config" "$HOME/.config/ghostty/config"
 
     # Git
-    link_file "$HOME/.dotfiles/src/dot_git/ignore" "$HOME/.config/git/ignore"
-    link_file "$HOME/.dotfiles/src/dot_git/.gitconfig" "$HOME/.gitconfig"
+    link_file "$DOTFILES/src/dot_git/ignore" "$HOME/.config/git/ignore"
+    link_file "$DOTFILES/src/dot_git/.gitconfig" "$HOME/.gitconfig"
 
     # Neovim
-    link_file "$HOME/.dotfiles/src/dot_nvim/init.lua" "$HOME/.config/nvim/init.lua"
-    link_file "$HOME/.dotfiles/src/dot_nvim/lua/plugins/background.lua" "$HOME/.config/nvim/lua/plugins/background.lua"
-    link_file "$HOME/.dotfiles/src/dot_nvim/lua/config/lazy.lua" "$HOME/.config/nvim/lua/config/lazy.lua"
-    link_file "$HOME/.dotfiles/src/dot_nvim/lua/config/kogut01.lua" "$HOME/.config/nvim/lua/config/kogut01.lua"
+    link_file "$DOTFILES/src/dot_nvim/init.lua" "$HOME/.config/nvim/init.lua"
+    link_file "$DOTFILES/src/dot_nvim/lua/plugins/background.lua" "$HOME/.config/nvim/lua/plugins/background.lua"
+    link_file "$DOTFILES/src/dot_nvim/lua/config/lazy.lua" "$HOME/.config/nvim/lua/config/lazy.lua"
+    link_file "$DOTFILES/src/dot_nvim/lua/config/kogut01.lua" "$HOME/.config/nvim/lua/config/kogut01.lua"
+
+    # Zsh
+    link_file "$DOTFILES/src/dot_zsh/.zshrc" "$HOME/.zshrc"
 
     # VsCode
     while read -r ext; do
       [[ -n "$ext" ]] && code --no-sandbox --force --install-extension "$ext"
       sleep 0.5
-    done < "$HOME/.dotfiles/src/dot_vscode/extensions.txt"
-    link_file "$HOME/.dotfiles/src/dot_vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+    done < "$DOTFILES/src/dot_vscode/extensions.txt"
+    link_file "$DOTFILES/src/dot_vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 
-    # Zsh
-    link_file "$HOME/.dotfiles/src/dot_zsh/.zshrc" "$HOME/.zshrc"
+    echo ""
+    echo "Instalacja zakończona! Do zobaczenia!"
 
 else
-    echo "Nieznany system: $OS"
+    echo "Implementacja dla systemu: $OS wkrótce..."
 fi
-
