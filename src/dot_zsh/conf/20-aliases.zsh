88209ca9d44d3807784a5621fb62dd4c80953139
Michał Popiel <131244219+Kogut01@users.noreply.github.com>
2026-06-24 09:01:54 +0200

    feat: change dir names and separate zsh config (#37)

---
+++ src/dot_zsh/conf/20-aliases.zsh
@@ -0,0 +1,13 @@
+# ----------
+# 0. ALIASES
+# ----------
+alias ls='lsd -l'
+alias la='lsd -la'
+alias lt='lsd --tree'
+
+alias vi='nvim'
+alias ff='fastfetch'
+
+alias ranger='spf'
+
+alias grep='grep --color=always'
