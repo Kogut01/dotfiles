a13711abaf81c3e69bdf9e1bbca02b0a6fffad68
Michał Popiel <131244219+Kogut01@users.noreply.github.com>
2026-06-24 11:52:36 +0200

    feat: add debian conf,brew bundle adn quickstart tutorial (#38)

--- src/dot_zsh/conf/00-env.zsh
+++ src/dot_zsh/conf/00-env.zsh
@@ -12,7 +12,17 @@ export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 
 
 # ------------
-# 1. Commands
+# 1. Homebrew
+# ------------
+if [[ -x "/opt/homebrew/bin/brew" ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [[ -f /etc/debian_version && -x "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+
+# ------------
+# 2. Commands
 # ------------
 bindkey -e
 ulimit -n 2048
