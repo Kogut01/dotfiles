f5ff20741e747a218eaeb4bff59f14f91390fce2
Michał Popiel <131244219+Kogut01@users.noreply.github.com>
2026-09-15 18:27:26 +0200

    refactor: some cleanups for repo

--- src/dot_zsh/conf/00-env.zsh
+++ src/dot_zsh/conf/00-env.zsh
@@ -3,16 +3,19 @@
 # -----------------
 export LANG="pl_PL.UTF-8"
 export TERM_PROGRAM="ghostty"
-export TERM="xterm-256color"
-export COLORTERM="truecolor"
-
 export PATH="$HOME/.local/bin:$PATH"
-export SSH_AUTH_SOCK="$HOME/.bitwarden-ssh-agent.sock"
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 
 
 # ------------
-# 1. Homebrew
+# 1. SSH Agent
+# ------------
+if [[ -S "$HOME/.bitwarden-ssh-agent.sock" && ( -z "$SSH_AUTH_SOCK" ||! _S "$SSH_AUTH_SOCK" ) ]]; then
+    export SSH_AUTH_SOCK="$HOME/.bitwarden-ssh-agent.sock"
+fi
+
+# ------------
+# 2. Homebrew
 # ------------
 if [[ -x "/opt/homebrew/bin/brew" ]]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -22,7 +25,7 @@ fi
 
 
 # ------------
-# 2. Commands
+# 3. Commands
 # ------------
 bindkey -e
 ulimit -n 2048
